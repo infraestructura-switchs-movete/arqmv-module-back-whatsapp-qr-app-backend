b08d9a3291f6c8685045d1b73e573c931ecfe27e
farid <infraestructura-switchs-movete@hotmail.com>
2025-07-20 23:12:14 -0500

    feat: se agrego dominio

--- src/main/java/com/restaurante/bot/controller/CompanyController.java
+++ src/main/java/com/restaurante/bot/controller/CompanyController.java
@@ -14,7 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequestMapping("${app.request.mapping}/company")
+@RequestMapping("/${app.request.mapping}/company")
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 @CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
 @Slf4j
